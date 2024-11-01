019edf78a4b092d46c26a40b41f602a5f9a33697
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2024-11-01 14:39:01 -0300

    update api.js e package.json

--- public/js/api.js
+++ public/js/api.js
@@ -1,15 +1,14 @@
 // Função para buscar as categorias e suas lives do backend
-export function fetchCategories() {
+export async function fetchCategories() {
   const url = "/api/getCategories";
 
-  return fetch(url)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Erro HTTP! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .catch((error) => {
-      console.error("Erro ao buscar as categorias:", error);
-    });
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Erro HTTP! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Erro ao buscar as categorias:", error);
+  }
 }
