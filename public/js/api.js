ccb72999e4b9ac46b7198e2f84af5055019e205b
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2024-11-01 14:20:43 -0300

    atualizar a api.js

--- public/js/api.js
+++ public/js/api.js
@@ -1,10 +1,14 @@
-// Função para buscar as categorias e suas lives do backend, com base no idioma
+// Função para buscar as categorias e suas lives do backend
 export function fetchCategories() {
-  // Define a URL da API com base no idioma recebido
-  const url = `/api/categories`;
+  const url = "/api/getCategories";
 
   return fetch(url)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Erro HTTP! Status: ${response.status}`);
+      }
+      return response.json();
+    })
     .catch((error) => {
       console.error("Erro ao buscar as categorias:", error);
     });
