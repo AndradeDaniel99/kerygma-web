5f34389d2e9e0373a082d80f924df3cbd30df8f9
Daniel de Andrade Souza <daniel.android.andrade@gmail.com>
2024-11-08 12:58:51 -0300

    update app.js para exibir visualizacoes formatado

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,31 @@ import { fetchCategories } from "./api.js";
 import { renderCategoryCarousels } from "./carousel.js";
 import { renderTabBar } from "./tabbar.js";
 
-// Função principal para carregar categorias e suas lives
-window.onload = () => {
-  // Renderiza a tabbar
-  renderTabBar();
+// Função para formatar o número de visualizações
+function formatViewersCount(viewersCount) {
+  return viewersCount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
 
-  // Chama a função para carregar os textos ao carregar a página
-  loadLocalization();
-
-  // Renderiza os carrosséis
-  fetchCategories().then((categories) => {
-    if (categories) {
-      renderCategoryCarousels(categories);
-    }
+// Função para formatar o JSON de categorias com a contagem de visualizações formatada
+function formatCategories(categories) {
+  return categories.map(category => {
+    const formattedLives = category.lives.map(live => ({
+      ...live,
+      viewers: formatViewersCount(live.viewers)
+    }));
+    return {
+      ...category,
+      lives: formattedLives
+    };
   });
-};
+}
+
+// Carrega as categorias e renderiza o carrossel
+async function loadAndRenderCategories() {
+  const categories = await fetchCategories();
+  const formattedCategories = formatCategories(categories);
+  renderCategoryCarousels(formattedCategories);
+}
 
 // Função para carregar o arquivo JSON de acordo com o idioma do usuário
 async function loadLocalization() {
@@ -35,3 +45,14 @@ async function loadLocalization() {
   document.querySelector(".footer .footer-copyright p").textContent =
     localization.footer.copyright;
 }
+
+// Chamando a função para carregar e renderizar as categorias ao carregar a página
+window.onload = () => {
+    // Renderiza a tabbar
+    renderTabBar();
+
+    // Chama a função para carregar os textos ao carregar a página
+    loadLocalization();
+
+  loadAndRenderCategories();
+};
